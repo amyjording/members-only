612b4e1b136767c6467d9f41f14281785d2086dd
Amy J <Amy J>
2017-02-06 21:48:31 -0800

    Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Wash",
+             email: "washfirefly@example.com",
+             password:                "foobar",
+             password_confirmation:   "foobar"
+             )
+
+User.create!(name:  "Mojo Jojo",
+             email: "jojomomo@example.com",
+             password:               "foobar",
+             password_confirmation:  "foobar"
+             )
+
+User.create!(name: "I Know",
+             email: "iknow@example.com",
+             password:              "bugsbunny",
+             password_confirmation: "bugsbunny"
+             )
